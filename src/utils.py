057776a4639bbe56fba9a63e9841bd97d3e01da9
winoo19 <danielsanchezs1903@gmail.com>
2024-04-08 19:49:44 +0200

    [utils] created utils with set_seed and to_double

---
+++ src/utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+import random
+import torch
+import os
+
+
+@torch.no_grad()
+def parameters_to_double(model: torch.nn.Module) -> None:
+    """
+    This function transforms the model parameters to double.
+
+    Args:
+        model: pytorch model.
+    """
+
+    for param in model.parameters():
+        print(param)
+        param.data = param.data.double()
+
+
+def set_seed(seed: int) -> None:
+    """
+    This function sets a seed and ensure a deterministic behavior.
+
+    Args:
+        seed: seed number to fix radomness.
+    """
+
+    # set seed in numpy and random
+    np.random.seed(seed)
+    random.seed(seed)
+
+    # set seed and deterministic algorithms for torch
+    torch.manual_seed(seed)
+    torch.use_deterministic_algorithms(True, warn_only=True)
+
+    # Ensure all operations are deterministic on GPU
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    # for deterministic behavior on cuda >= 10.2
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+
+    return None

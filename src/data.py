99e6f8084726d2cada7aa33d36c803da24b638e1
Sergio Herreros <81583645+goomii17@users.noreply.github.com>
2024-04-09 09:44:51 +0200

    Implemented download data

--- src/data.py
+++ src/data.py
@@ -1,91 +1,79 @@
 # deep learning libraries
-import torch
 from torch.utils.data import Dataset, DataLoader
-
-
-# own libraries
-from utils import set_seed
 
 
 # other libraries
 import pypianoroll as ppr
 import numpy as np
 import os
+import requests
+import zipfile
+import shutil
+from tqdm import tqdm
 
 
-set_seed(42)
+DATA_PATH = "data"
+RESOLUTION = 8
 
 
-class PianoRollDataset(Dataset):
+def download_data() -> None:
     """
-    This class represents a dataset of piano roll representations of songs.
-
-    Attributes:
-        dataset (torch.Tensor): The binary representation of the songs.
+    Load the MAESTRO MIDI dataset from the web.
     """
 
-    dataset: torch.Tensor
+    url = "https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0-midi.zip"
 
-    def __init__(self, dataset: list[np.ndarray], bar_length: int = 16) -> None:
-        """
-        Initializes the PianoRollDataset.
+    response = requests.get(url)
 
-        Args:
-            dataset (list[np.ndarray]): The binary representation of the songs.
-            It has the shape (n_songs, n_timesteps (different for each song), n_pitches).
-            bar_length (int): The length of a bar in beats.
-        """
+    temp_path = os.path.join(DATA_PATH, "temp")
+    zip_file_path = os.path.join(temp_path, "maestro-v3.0.0-midi.zip")
 
-        self.bar_length = bar_length
+    # Create temp folder
+    os.makedirs(temp_path, exist_ok=True)
 
-        self.silence = torch.zeros((self.bar_length, dataset[0].shape[1]))
+    with open(zip_file_path, "wb") as f:
+        f.write(response.content)
 
-        self.dataset = [bar for track in dataset for bar in self.process_bars(track)]
+    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+        zip_ref.extractall(temp_path)
 
-    def process_bars(self, track: np.ndarray) -> list:
-        """
-        Process a track splitting it into bars.
+    # Create midi folder
+    os.makedirs(os.path.join(DATA_PATH, "midi"), exist_ok=True)
 
-        Args:
-            track (np.ndarray): The piano roll representation of the song.
+    # Loop all folders and move files to data folder
+    for root, _, files in os.walk(temp_path):
+        for file in files:
+            if file.endswith(".midi"):
+                os.rename(
+                    os.path.join(root, file), os.path.join(DATA_PATH, "midi", file)
+                )
 
-        Returns:
-            list: The piano roll representation of the song split into bars.
-        """
+    # Remove temp folder
+    shutil.rmtree(temp_path)
 
-        n_bars = track.shape[0] // self.bar_length
-        new_track = torch.concatenate(
-            self.silence
-            + [
-                torch.tensor(track[i * self.bar_length : (i + 1) * self.bar_length])
-                for i in range(n_bars)
-            ]
-        )
 
-        return list(zip(new_track, new_track[1:]))
+def transform_data() -> None:
+    """
+    This method transforms the data from midi to piano roll and saves it as a numpy file.
+    """
 
-    def __len__(self) -> int:
-        """
-        Returns the length of the dataset.
+    midi_path = os.path.join(DATA_PATH, "midi")
+    npy_path = os.path.join(DATA_PATH, "npy")
 
-        Returns:
-            int: The number of tracks in the dataset.
-        """
+    if not os.path.exists(midi_path):
+        print("Data is not downloaded. Downloading data...")
+        download_data()
 
-        return len(self.dataset)
+    os.makedirs(npy_path, exist_ok=True)
 
-    def __getitem__(self, index: int) -> tuple[torch.Tensor, torch.Tensor]:
-        """
-        Returns an element from the dataset based on the index.
+    pbar = tqdm(os.listdir(midi_path))
 
-        Args:
-            index (int): The index of the element.
+    for i, file in enumerate(pbar):
+        track = ppr.read(os.path.join(midi_path, file), resolution=RESOLUTION)
+        file_name = f"pianoroll_{i}.npy"
+        np.save(os.path.join(npy_path, file_name), track.tracks[0].pianoroll)
 
-        Returns:
-            tuple[torch.Tensor, torch.Tensor]: The former and input bars of the song.
-        """
-
-        return self.dataset[index]
+    print("Data successfully transformed!")
 
 
 def load_data(
@@ -93,45 +81,15 @@ def load_data(
     shuffle: bool = True,
     drop_last: bool = True,
     num_workers: int = 0,
-    train_p: float = 0.7,
-    val_p: float = 0.15,
-) -> tuple[DataLoader, DataLoader, DataLoader]:
+) -> DataLoader:
     """
-    This method returns Dataloaders of the chosen dataset.
-
-    Args:
-        batch_size (int): The size of the batch.
-        shuffle (bool): Whether to shuffle the data.
-        drop_last (bool): Whether to drop the last batch if it's smaller than the batch size.
-        num_workers (int): The number of worker threads for data loading.
-        train_p (float): The percentage of data used for training.
-        val_p (float): The percentage of data used for validation.
-
-    Returns:
-        tuple[DataLoader, DataLoader, DataLoader]: The train, validation, and test dataloaders.
+    This method a dataloader for the dataset.
     """
 
-    # Transform and load the data
-    files = transform_data()
-
-    # separate the data into train, validation and test
-    files = list(np.random.permutation(files))
-
-    n_files = len(files)
-    n_train = int(n_files * train_p)
-    n_val = int(n_files * val_p)
-
-    df_train = files[:n_train]
-    df_val = files[n_train : n_train + n_val]
-    df_test = files[n_train + n_val :]
-
-    # Create datasets
-    train_dataset = PianoRollDataset(df_train)
-    val_dataset = PianoRollDataset(df_val)
-    test_dataset = PianoRollDataset(df_test)
+    train_dataset = Dataset()
 
     # Create dataloaders
-    train_loader = DataLoader(
+    data_loader = DataLoader(
         train_dataset,
         batch_size=batch_size,
         shuffle=shuffle,
@@ -139,80 +97,9 @@ def load_data(
         num_workers=num_workers,
     )
 
-    val_loader = DataLoader(
-        val_dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-        drop_last=drop_last,
-        num_workers=num_workers,
-    )
-
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-        drop_last=drop_last,
-        num_workers=num_workers,
-    )
-
-    return train_loader, val_loader, test_loader
-
-
-def transform_data() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """
-    This method transforms the data to be used in the model.
-
-    Returns:
-        train data.
-        val data.
-        test data.
-    """
-
-    n_files = len(os.listdir("data/midis"))
-
-    if not os.path.exists("data/npy_files"):
-
-        os.makedirs("data/npy_files")
-
-        for i, file in enumerate(os.listdir("data/midis")):
-            if file.endswith(".mid"):
-                try:
-                    track = ppr.read("data/midis/" + file)
-
-                except Exception as e:
-                    print(f"{file} could not be read")
-                    print(e)
-                    continue
-
-                if len(track.tracks) != 1:
-                    print(f"{file} has more than one track")
-                    continue
-
-                track = track.binarize()
-
-                np.save(f"data/npy_files/{file[:-4]}.npy", track.tracks[0].pianoroll)
-
-            else:
-                print(f"{file} is not a midi file")
-
-            if i % 100 == 0:
-                print(f"{i}/{n_files} files processed")
-
-    files: list[np.ndarray] = []
-
-    for file in enumerate(os.listdir("data/train")):
-        files.append(np.load("data/npy_files/" + file))
-
-    return files
+    return data_loader
 
 
 if __name__ == "__main__":
-    files = transform_data(0.7, 0.15)
-
-    for i in range(20):
-        track = files[i]
-        print(f"Track {i+1}:")
-        print(track)
-        print()
-
-    print(f"n files: {len(files)}")
+    # download_data()
+    transform_data()
